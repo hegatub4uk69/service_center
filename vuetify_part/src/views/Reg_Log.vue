82e927e86e1bb886fa906cce65b73b2b7bcc77ed
hegatub4uk69 <serzh.dimitrov3@mail.ru>
2023-10-17 11:32:28 +0800

    Changes to be committed: 	modified:   vuetify_part/src/views/Reg_Log.vue

--- vuetify_part/src/views/Reg_Log.vue
+++ vuetify_part/src/views/Reg_Log.vue
@@ -1,69 +1,70 @@
 <template>
   <v-card class="fill-height">
-    <v-row
-      style=""
-    >
-      <v-col>
-        <v-form
-          v-model="form"
-          @submit.prevent="onSubmit"
-        >
-          <v-card-text>
-            <v-text-field
-              v-model="login"
-              :readonly="loading"
-              :rules="[required]"
-              clearable
-              label="Username"
-              color="primary"
-              prepend-inner-icon="mdi-account-circle-outline"
-              hint="Enter your login to access to website"
-            />
-            <v-text-field
-              v-model="password"
-              :readonly="loading"
-              :rules="[required]"
-              :type="showPassword ? 'text' : 'Password'"
-              clearable
-              label="Password"
-              color="primary"
-              prepend-inner-icon="mdi-lock-outline"
-              hint="Enter your password to access to website"
-              :append-inner-icon="showPassword ? 'mdi-eye-outline' : 'mdi-eye-off-outline' "
-              @click:append-inner="showPassword = !showPassword"
-            />
-
-          </v-card-text>
-          <v-card-actions>
-            <v-btn
-              v-model="register"
-              :disabled="!form"
-              :loading="loading"
-              type="submit"
-              variant="elevated"
-              color="info"
-              size="large"
-              class="ma-2"
-            >
-              Регистрация
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn
-              v-model="authorize"
-              :disabled="!form"
-              :loading="loading"
-              type="submit"
-              variant="elevated"
-              color="success"
-              size="large"
-              class="ma-2"
-            >
-              Авторизация
-            </v-btn>
-          </v-card-actions>
-        </v-form>
-      </v-col>
-    </v-row>
+    <v-container>
+      <v-row no-gutters=""
+      style="height: 800px; width: 1800px"
+      >
+        <v-col></v-col>
+        <v-col align-self="center">
+          <v-form
+            v-model="form"
+            @submit.prevent="onSubmit"
+          >
+            <v-card-text>
+              <v-text-field
+                v-model="login"
+                :readonly="loading"
+                :rules="[required]"
+                clearable="true"
+                label="Username"
+                color="primary"
+                prepend-inner-icon="mdi-account-circle-outline"
+                hint="Enter your login to access to website"
+              />
+              <v-text-field
+                v-model="password"
+                :readonly="loading"
+                :rules="[required]"
+                :type="showPassword ? 'text' : 'Password'"
+                clearable="true"
+                label="Password"
+                color="primary"
+                prepend-inner-icon="mdi-lock-outline"
+                hint="Enter your password to access to website"
+                :append-inner-icon="showPassword ? 'mdi-eye-outline' : 'mdi-eye-off-outline' "
+                @click:append-inner="showPassword = !showPassword"
+              />
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                v-model="register"
+                :disabled="!form"
+                :loading="loading"
+                type="submit"
+                variant="elevated"
+                color="info"
+                size="large"
+                class="ma-2"
+              >Регистрация
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn
+                v-model="authorize"
+                :disabled="!form"
+                :loading="loading"
+                type="submit"
+                variant="elevated"
+                color="success"
+                size="large"
+                class="ma-2"
+              >Авторизация
+              </v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-col>
+        <v-col></v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
 
